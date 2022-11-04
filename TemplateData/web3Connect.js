bd7d34b54e23d6afff01d4a017fc7b472a56ff3c
leon-do <leondo123@gmail.com>
2022-11-04 00:43:36 -0700

    rm web3

--- TemplateData/web3Connect.js
+++
@@ -1,7 +0,0 @@
-if (window.ethereum) {
-  web3 = new Web3(window.ethereum);
-  // connect popup
-  ethereum.enable();
-} else {
-  // alert("Non-Ethereum browser detected. Please connect to a wallet");
-}
